e45481679b8d341d87e4c831a287eff739d57b8b
Jeffreytsai1004 <109943015+Jeffreytsai1004@users.noreply.github.com>
2024-03-11 21:58:58 +0800

    Update

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -12,16 +12,9 @@ export default navbar([
         icon: "pen-to-square",
         prefix: "UnrealEngine/",
         children: [
-          {
-            text: "UnrealGameSync",
-            icon: "pen-to-square",
-            prefix: "UnrealGameSync/",
-            children: [
-              { text: "UGS Build & Publish 4.27", icon: "pen-to-square", link: "0001" },
-              { text: "UGS Build & Publish 5.3.2", icon: "pen-to-square", link: "0002" },
-              { text: "UGS MySQL Deployment", icon: "pen-to-square", link: "0003" },
-            ],
-          },
+          { text: "UGS Build & Publish 4.27", icon: "pen-to-square", link: "0001" },
+          { text: "UGS Build & Publish 5.3.2", icon: "pen-to-square", link: "0002" },
+          { text: "UGS MySQL Deployment", icon: "pen-to-square", link: "0003" },
         ],
       },
     ],
--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -14,16 +14,9 @@ export default sidebar({
           icon: "pen-to-square",
           prefix: "UnrealEngine/",
           children: [
-            {
-              text: "UnrealGameSync",
-              icon: "pen-to-square",
-              prefix: "UnrealGameSync/",
-              children: [
-                { text: "UGS Build & Publish 4.27", icon: "pen-to-square", link: "0001" },
-                { text: "UGS Build & Publish 5.3.2", icon: "pen-to-square", link: "0002" },
-                { text: "UGS MySQL Deployment", icon: "pen-to-square", link: "0003" },
-              ],
-            },
+            { text: "UGS Build & Publish 4.27", icon: "pen-to-square", link: "0001" },
+            { text: "UGS Build & Publish 5.3.2", icon: "pen-to-square", link: "0002" },
+            { text: "UGS MySQL Deployment", icon: "pen-to-square", link: "0003" },
           ],
         },
       ],
