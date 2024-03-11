6f07344c7e944e122a4c8aa65e45d25d47038cd5
Jeffreytsai1004 <109943015+Jeffreytsai1004@users.noreply.github.com>
2024-03-11 21:50:57 +0800

    Update navbar.ts

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -13,7 +13,8 @@ export default navbar([
         prefix: "UnrealEngine/",
         children: [
           { text: "UGS Build & Publish 4.27", icon: "pen-to-square", link: "0001" },
-          { text: "UGS MySQL Deployment", icon: "pen-to-square", link: "0002" },
+          { text: "UGS Build & Publish 5.3.2", icon: "pen-to-square", link: "0002" },
+          { text: "UGS MySQL Deployment", icon: "pen-to-square", link: "0003" },
         ],
       },
     ],
