6f09f43dd4336f7a1788289f457d2685efaccf03
Jeffreytsai1004 <109943015+Jeffreytsai1004@users.noreply.github.com>
2024-03-11 22:55:22 +0800

    Update

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -15,6 +15,7 @@ export default navbar([
           { text: "UGS Build & Publish 4.27", icon: "pen-to-square", link: "0001" },
           { text: "UGS Build & Publish 5.3.2", icon: "pen-to-square", link: "0002" },
           { text: "UGS MySQL Deployment", icon: "pen-to-square", link: "0003" },
+          { text: "UGS MetadataServer .NET Core", icon: "pen-to-square", link: "0004" },
         ],
       },
     ],
