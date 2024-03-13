401f0a6289f67067e2b2f3cc295313f605d97c18
Jeffreytsai1004 <109943015+Jeffreytsai1004@users.noreply.github.com>
2024-03-14 02:00:58 +0800

    Update

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -8,31 +8,30 @@ export default navbar([
     children: [
       {
         text: "UnrealEngine",
-        icon: "pen-to-square",
+        icon: "unrealengine",
         prefix: "UnrealEngine/",
         children: [
-          { text: "UGS Build & Publish 4.27", icon: "pen-to-square", link: "0001" },
-          { text: "UGS Build & Publish 5.3.2", icon: "pen-to-square", link: "0002" },
-          { text: "UGS MySQL Deployment", icon: "pen-to-square", link: "0003" },
-          { text: "UGS MetadataServer .NET Core", icon: "pen-to-square", link: "0004" },
-          { text: "Metahuman 基数设置", icon: "pen-to-square", link: "0005" },
+          { text: "UGS Build & Publish 4.27", icon: "unreal-engine", link: "0001" },
+          { text: "UGS Build & Publish 5.3.2", icon: "unreal-engine", link: "0002" },
+          { text: "UGS MySQL Deployment", icon: "unreal-engine", link: "0003" },
+          { text: "UGS MetadataServer .NET Core", icon: "unrealengine", link: "0004" },
         ],
       },
       {
         text: "MetaHuman",
-        icon: "pen-to-square",
+        icon: "unrealengine",
         prefix: "MetaHuman/",
         children: [
-          { text: "Metahuman 基数设置", icon: "pen-to-square", link: "0001" },
+          { text: "Metahuman 基数设置", icon: "unreal-engine", link: "0001" },
         ],
       },
       {
         text: "UEFN",
-        icon: "pen-to-square",
+        icon: "brand-fortnite",
         prefix: "UEFN/",
         children: [
-          { text: "虚幻编辑器《堡垒之夜》文档", icon: "pen-to-square", link: "0001" },
-          { text: "《堡垒之夜》虚幻编辑器的新功能" , icon: "pen-to-square", link: "0002" },
+          { text: "虚幻编辑器《堡垒之夜》文档", icon: "fortnite", link: "0001" },
+          { text: "《堡垒之夜》虚幻编辑器的新功能" , icon: "fortnite", link: "0002" },
         ],
       },
     ],
